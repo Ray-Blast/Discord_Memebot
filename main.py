8553ceb1fd09fee9a7e0377d6c00049239a6ae9a
Braulio Pimentel <BraulioPimentel@users.noreply.github.com>
2022-12-24 18:29:44 -0600

    Additional Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import discord
+from discord.ext import commands
+import requests
+
+
 
 if __name__ == '__main__':
-    pass
+
+    TOKEN = 'MTA1NjM2MjY5Mzg4OTYzNDMxNA.GfIWi9.i5jPQo8PE9XHIbMWlli65mXO9y99EH8rT27RGc'
+    intents = discord.Intents.default()
+    intents.members = True
+    client = commands.Bot(command_prefix='!', intents = intents)
+
+    
