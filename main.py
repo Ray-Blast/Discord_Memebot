bf1c0d988cd8fbd196aab071c79d47d5be8bd723
Braulio Pimentel <BraulioPimentel@users.noreply.github.com>
2022-12-27 00:57:22 -0600

    com2

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import requests
 
 if __name__ == '__main__':
 
-    TOKEN = 'MTA1NjM2MjY5Mzg4OTYzNDMxNA.GfIWi9.i5jPQo8PE9XHIbMWlli65mXO9y99EH8rT27RGc'
+    TOKEN = 'MTA1NjM2MjY5Mzg4OTYzNDMxNA.GzDUK4.0BLIN6BSx0biFcivSSz4zv-Mzdpc1nlGl79RdI'
     intents = discord.Intents.default()
     intents.members = True
     client = commands.Bot(command_prefix='!', intents = intents)
 
     
+
+client.run(TOKEN)
