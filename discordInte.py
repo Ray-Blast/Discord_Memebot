0b54ebf584c1cf9947c9bb022b08a38bb4565c0e
BraulioPimentel <56987789+BraulioPimentel@users.noreply.github.com>
2023-03-28 14:22:51 -0500

    added requirements.txt, adding functionality to the integration file

--- discordInte.py
+++ discordInte.py
@@ -1,9 +1,17 @@
 import discord
 import os
+from dotenv import load_dotenv
 from discord.ext import commands
 
+load_dotenv()#loads files from a .dotenv
+
+# the 'main' function of the discord integration stuff. executes all the discord functionality
 def startDiscordInte():
     TOKEN = os.getenv("DISCORD_TOKEN")
     intents = discord.Intents.default()
     intents.members = True
     client = commands.Bot(command_prefix='!', intents = intents)
+
+    @client.event
+    async def on_ready():
+        print("we are logged in")
