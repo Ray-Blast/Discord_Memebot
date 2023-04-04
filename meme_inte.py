ad49f6b404a7f11c8d7425c7569d90c360bcbf09
BraulioPimentel <56987789+BraulioPimentel@users.noreply.github.com>
2023-04-04 12:55:25 -0500

    Added Basic Interactions between bot and imgur, working on using library soon

--- meme_inte.py
+++ meme_inte.py
@@ -1,10 +1,15 @@
 import requests
+import random
 
-
+#placeholder
 
 
 def getMeme():
-    meme_url = "https://twitter.com/i/status/1641422683924512769"
+    
+    memeURLs = ["https://i.imgur.com/cqVMHfX.jpg", "https://i.imgur.com/qDiMN4n.jpg", "https://i.imgur.com/xrIl15t.jpg"]
+
+    meme_url = random.choice(memeURLs)
+    #print(meme_url)
 
     return requests.get(meme_url)
 
